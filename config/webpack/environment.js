07602f6c038ea069d845297efedfa76c0c109621
Nathan Hopkins <natehop@gmail.com>
2018-10-23 09:42:20 -0600

    Got ProvidePlugin working so we can load theme js files

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment;
