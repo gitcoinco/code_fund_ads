e2b0074845c01eeab71adb07ed96f41c93988775
Nathan Hopkins <natehop@gmail.com>
2018-11-15 11:50:02 -0700

    Update comments

--- app/models/authorized_user.rb
+++ app/models/authorized_user.rb
@@ -3,7 +3,15 @@
 class AuthorizedUser < Perm::Authorized
   alias_method :user, :subject
 
-  # IMPORTANT: Authorization methods in concerns must be unique to avoid naming collisions
+  # Authorization methods in concerns must be unique to avoid naming collisions
+  # The naming convention is: can_VERB_NOUN?
+  #
+  # Examples:
+  #
+  #   can_admin_system?
+  #   can_update_image?
+  #   can_view_imageable?
+  #
   include Authorizers::Roles
   include Authorizers::Images
   include Authorizers::Imageable
