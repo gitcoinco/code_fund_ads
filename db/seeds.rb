5e6fd0a9cf84e77d02237063f14bfd7814673805
Ron Cooke <ron@astonspark.com>
2018-11-20 10:48:23 -0500

    Expand data seed (#26)
    Expand data seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,134 +1,279 @@
+# frozen_string_literal: true
+
+require "csv"
+require "fileutils"
 require "faker"
+
+require_relative "./seeds_impressions"
 
 unless Rails.env.development?
   puts "SEEDS ARE FOR DEVELOPMENT ONLY!"
   exit 1
 end
 
-user_attributes = {
-  company_name: "CodeFund",
-  confirmation_sent_at: 2.days.ago,
-  confirmed_at: 1.day.ago,
-  password: "secret",
-  password_confirmation: "secret",
-}
+class Seeder
+  def self.run
+    new.call
+  end
 
-administrator = User.find_or_initialize_by(email: "chris.knight@codefund.io")
-administrator.assign_attributes(
-  user_attributes.merge(
-    roles: [ENUMS::USER_ROLES::ADMINISTRATOR],
-    first_name: "Chris",
-    last_name: "Knight",
-  )
-)
-administrator.save!
+  def initialize
+    @user_id = User.last&.id.to_i + 1
+    @property_id = Property.last&.id.to_i + 1
+    @user_count = 0
+    @users = []
+    @publishers = []
+    @properties = []
+  end
 
-publisher = User.find_or_initialize_by(email: "mitch.taylor@codefund.io")
-publisher.assign_attributes(
-  user_attributes.merge(
-    roles: [ENUMS::USER_ROLES::PUBLISHER],
-    first_name: "Mitch",
-    last_name: "Taylor",
-  )
-)
-publisher.save!
+  def call
+    puts "[SEEDING DATABASE]: Started - This may take several minutes."
+    seed_users
+    seed_campaigns
+    seed_properties
+    ImpressionSeeder.run(ENV["MAX_IMPRESSIONS"].to_i)
+    puts "[SEEDING DATABASE]: Completed"
+  end
 
-advertiser = User.find_or_initialize_by(email: "jordan.cochran@codefund.io")
-advertiser.assign_attributes(
-  user_attributes.merge(
-    roles: [ENUMS::USER_ROLES::ADVERTISER],
-    first_name: "Jordan",
-    last_name: "Cochran",
-  )
-)
-advertiser.save!
+  private
 
-if advertiser.images.search_metadata_name("CodeFund Small").search_metadata_format(ENUMS::IMAGE_FORMATS::SMALL).count == 0
-  advertiser.images.attach io: File.open(Rails.root.join("app/assets/images/seeds/code-fund-100x100.png")),
-    filename: "code-fund-100x100.png",
-    content_type: "image/png",
-    metadata: {
-      identified: true,
-      width: 100,
-      height: 100,
-      analyzed: true,
-      name: "CodeFund Small",
-      format: ENUMS::IMAGE_FORMATS::SMALL,
+  def seed_users
+    @emails = User.all.pluck(:email).each_with_object({}) { |email, memo|
+      memo[email] = true
     }
-end
+    csv_path = Rails.root.join("tmp", "users.csv")
+    @users << build_administrator
+    @users.concat build_advertisers
+    @users.concat build_publishers
+    csv_created = create_csv(@users, csv_path)
+    import_csv("users", csv_path) if csv_created
+  end
 
-if advertiser.images.search_metadata_name("CodeFund Large").search_metadata_format(ENUMS::IMAGE_FORMATS::LARGE).count == 0
-  advertiser.images.attach io: File.open(Rails.root.join("app/assets/images/seeds/code-fund-260x200.png")),
-    filename: "code-fund-100x100.png",
-    content_type: "image/png",
-    metadata: {
-      identified: true,
-      width: 260,
-      height: 200,
-      analyzed: true,
-      name: "CodeFund Large",
-      format: ENUMS::IMAGE_FORMATS::LARGE,
+  def build_administrator
+    admin_count = User.where("? = ANY (roles)", ENUMS::USER_ROLES::ADMINISTRATOR).count
+    return unless admin_count.zero?
+    return if @emails["admin@codefund.io"]
+    puts "[SEEDING DATABASE]: Building administrator"
+    @user_count += 1
+    admin = user_attributes.merge(
+      id: @user_id,
+      company_name: "CodeFund",
+      email: "admin@codefund.io",
+      roles: "{#{ENUMS::USER_ROLES::ADMINISTRATOR}}",
+    ).values
+    @user_id += 1
+    admin
+  end
+
+  def build_advertisers
+    advertiser_count = User.where("? = ANY (roles)", ENUMS::USER_ROLES::ADVERTISER).count
+    return [] if advertiser_count >= 100
+    print "[SEEDING DATABASE]: Building advertisers "
+    advertisers = (advertiser_count..99).map {
+      @user_count += 1
+      print "." if @user_count % 10 == 0
+      advertiser = user_attributes.merge(id: @user_id, roles: "{#{ENUMS::USER_ROLES::ADVERTISER}}").values
+      @user_id += 1
+      advertiser
     }
-end
+    puts
+    advertisers
+  end
 
-if advertiser.creatives.count == 0
-  5.times do
-    creative = Creative.create(
-      user: advertiser,
-      name: Faker::SiliconValley.company,
-      headline: Faker::SiliconValley.invention,
-      body: Faker::SiliconValley.motto,
-    )
-    advertiser.images.each do |image|
-      CreativeImage.create creative: creative, image: image
+  def build_publishers
+    publisher_count = User.where("? = ANY (roles)", ENUMS::USER_ROLES::PUBLISHER).count
+    return [] if publisher_count >= 1000
+    print "[SEEDING DATABASE]: Building publishers "
+    publishers = (publisher_count..999).map {
+      @user_count += 1
+      print "." if @user_count % 10 == 0
+      publisher = user_attributes.merge(id: @user_id, roles: "{#{ENUMS::USER_ROLES::PUBLISHER}}").values
+      @publishers << @user_id
+      @user_id += 1
+      publisher
+    }
+    puts
+    publishers
+  end
+
+  def seed_campaigns
+    return if Campaign.count >= 1000
+    print "[SEEDING DATABASE]: Seeding campaigns "
+    User.where("? = ANY (roles)", ENUMS::USER_ROLES::ADVERTISER).each do |advertiser|
+      print "."
+      add_small_image(advertiser)
+      add_large_image(advertiser)
+      generate_creatives(advertiser)
+      generate_campaigns(advertiser)
+    end
+    puts
+  end
+
+  def add_small_image(advertiser)
+    advertiser.images.attach io: File.open(Rails.root.join("app/assets/images/seeds/code-fund-100x100.png")),
+      filename: "code-fund-100x100.png",
+      content_type: "image/png",
+      metadata: {
+        identified: true,
+        width: 100,
+        height: 100,
+        analyzed: true,
+        name: "CodeFund Small",
+        format: ENUMS::IMAGE_FORMATS::SMALL,
+      }
+  end
+
+  def add_large_image(advertiser)
+    advertiser.images.attach io: File.open(Rails.root.join("app/assets/images/seeds/code-fund-260x200.png")),
+      filename: "code-fund-100x100.png",
+      content_type: "image/png",
+      metadata: {
+        identified: true,
+        width: 260,
+        height: 200,
+        analyzed: true,
+        name: "CodeFund Large",
+        format: ENUMS::IMAGE_FORMATS::LARGE,
+      }
+  end
+
+  def generate_creatives(advertiser)
+    5.times do
+      creative = Creative.create(
+        user: advertiser,
+        name: Faker::SiliconValley.company,
+        headline: Faker::SiliconValley.invention,
+        body: Faker::SiliconValley.motto,
+      )
+      advertiser.images.each do |image|
+        CreativeImage.create creative: creative, image: image
+      end
     end
   end
-end
 
-if advertiser.campaigns.count == 0
-  10.times do
-    start_date = rand(3).months.from_now.to_date
-    end_date = start_date.advance(months: 6)
-    total_budget = ([*500..5000].sample / 100) * 100
-    daily_budget = total_budget / (end_date - start_date).to_i
-    countries = ENUMS::DEVELOPED_MARKET_COUNTRIES.keys
-    countries += ENUMS::EMERGING_MARKET_COUNTRIES.keys if rand(3).zero?
-    countries += ENUMS::COUNTRIES.keys.sample(5) if rand(5).zero?
-    keywords = ENUMS::KEYWORDS.values.sample(25)
-    Campaign.create(
-      user: advertiser,
-      creative: advertiser.creatives.sample,
-      status: ENUMS::CAMPAIGN_STATUSES.values.sample,
-      name: Faker::SiliconValley.invention,
-      url: Faker::SiliconValley.url,
-      start_date: start_date,
-      end_date: end_date,
-      us_hours_only: rand(2).zero?,
-      weekdays_only: rand(2).zero?,
-      total_budget: total_budget,
-      daily_budget: daily_budget,
-      ecpm: 3,
-      countries: countries,
-      keywords: keywords,
-      negative_keywords: ENUMS::KEYWORDS.values.sample(5) - keywords,
-    )
+  def generate_campaigns(advertiser)
+    10.times do
+      start_date = rand(3).months.from_now.to_date
+      end_date = start_date.advance(months: 6)
+      total_budget = ([*500..5000].sample / 100) * 100
+      daily_budget = total_budget / (end_date - start_date).to_i
+      countries = ENUMS::DEVELOPED_MARKET_COUNTRIES.keys
+      countries += ENUMS::EMERGING_MARKET_COUNTRIES.keys if rand(3).zero?
+      countries += ENUMS::COUNTRIES.keys.sample(5) if rand(5).zero?
+      keywords = ENUMS::KEYWORDS.values.sample(25)
+      Campaign.create(
+        user: advertiser,
+        creative: advertiser.creatives.sample,
+        status: ENUMS::CAMPAIGN_STATUSES.values.sample,
+        name: Faker::SiliconValley.invention,
+        url: Faker::SiliconValley.url,
+        start_date: start_date,
+        end_date: end_date,
+        us_hours_only: rand(2).zero?,
+        weekdays_only: rand(2).zero?,
+        total_budget: total_budget,
+        daily_budget: daily_budget,
+        ecpm: 3,
+        countries: countries,
+        keywords: keywords,
+        negative_keywords: ENUMS::KEYWORDS.values.sample(5) - keywords,
+      )
+    end
+  end
+
+  def seed_properties
+    return if Property.count >= 10_000
+    print "[SEEDING DATABASE]: Building properties"
+    csv_path = Rails.root.join("tmp", "properties.csv")
+    @publishers = User.where("? = ANY (roles)", ENUMS::USER_ROLES::PUBLISHER).pluck(:id) unless @publishers.present?
+    @publishers.each do |publisher|
+      print "."
+      10.times do
+        @properties << {
+          id: @property_id,
+          user_id: publisher,
+          property_type: ENUMS::PROPERTY_TYPES.values.sample,
+          status: ENUMS::PROPERTY_STATUSES.values.sample,
+          name: Faker::SiliconValley.invention,
+          description: nil,
+          url: Faker::SiliconValley.url,
+          ad_template: ENUMS::AD_TEMPLATES.values.sample,
+          ad_theme: ENUMS::AD_THEMES.values.sample,
+          language: ENUMS::LANGUAGES::ENGLISH,
+          keywords: "{#{ENUMS::KEYWORDS.values.sample(25).join(",")}}",
+          prohibited_advertisers: nil,
+          prohibit_fallback_campaigns: false,
+          created_at: Time.now,
+          updated_at: Time.now,
+        }.values
+        @property_id += 1
+      end
+    end
+    puts
+    csv_created = create_csv(@properties, csv_path)
+    import_csv("properties", csv_path) if csv_created
+  end
+
+  def create_csv(list, csv_path)
+    return false unless list.compact.present?
+    puts "Creating #{csv_path}"
+    CSV.open(csv_path, "wb") do |csv|
+      list.each do |record|
+        csv << record
+      end
+    end
+    true
+  end
+
+  def import_csv(table_name, csv_path)
+    puts "Copying #{csv_path} to Postgres"
+    ActiveRecord::Base.connection.execute "COPY \"#{table_name}\" FROM '#{csv_path}' CSV;"
+    puts "Copy complete"
+  rescue => e
+    puts "Failed to copy #{csv_path} to Postgres! #{e}"
+  ensure
+    FileUtils.rm_f csv_path, verbose: true
+  end
+
+  def user_attributes
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    email = Faker::Internet.email("#{first_name} #{last_name}", "-")
+    {
+      id: 1,
+      roles: [],
+      first_name: first_name,
+      last_name: last_name,
+      company_name: Faker::SiliconValley.company,
+      address_1: nil,
+      address_2: nil,
+      city: nil,
+      region: nil,
+      postal_code: nil,
+      country: nil,
+      api_access: true,
+      api_key: nil,
+      paypal_email: nil,
+      email: email,
+      encrypted_password: User.new(password: "secret").encrypted_password,
+      reset_password_token: nil,
+      reset_password_sent_at: nil,
+      remember_created_at: nil,
+      sign_in_count: 1,
+      current_sign_in_at: nil,
+      last_sign_in_at: nil,
+      current_sign_in_ip: nil,
+      last_sign_in_ip: nil,
+      confirmation_token: Devise.friendly_token,
+      confirmed_at: 1.day.ago,
+      confirmation_sent_at: 2.days.ago,
+      unconfirmed_email: nil,
+      failed_attempts: 0,
+      unlock_token: nil,
+      locked_at: nil,
+      created_at: 3.days.ago,
+      updated_at: 3.days.ago,
+    }
   end
 end
 
-if publisher.properties.count == 0
-  10.times do
-    Property.create(
-      user: publisher,
-      property_type: ENUMS::PROPERTY_TYPES.values.sample,
-      status: ENUMS::PROPERTY_STATUSES.values.sample,
-      name: Faker::SiliconValley.invention,
-      url: Faker::SiliconValley.url,
-      ad_template: ENUMS::AD_TEMPLATES.values.sample,
-      ad_theme: ENUMS::AD_THEMES.values.sample,
-      language: ENUMS::LANGUAGES::ENGLISH,
-      keywords: ENUMS::KEYWORDS.values.sample(25),
-    )
-  end
-end
-
-require_relative "./seeds_impressions" if ENV["IMPRESSIONS"]
+Seeder.run
--- db/seeds_impressions.rb
+++ db/seeds_impressions.rb
@@ -1,107 +1,122 @@
+# frozen_string_literal: true
+
 require "csv"
 require "fileutils"
 
-@publisher = User.publisher.where(first_name: "Mitch", last_name: "Taylor").first
-@campaign_cache = {}
-
-def build_impression(displayed_at)
-  property = @publisher.properties.sample
-  campaign = @campaign_cache[property.id] || Campaign.for_property(property).limit(1).first
-  @campaign_cache[property.id] = campaign
-  return nil unless campaign
-  {
-    id: SecureRandom.uuid,
-    campaign_id: campaign.id,
-    property_id: property.id,
-    ip: rand(6).zero? ? Faker::Internet.ip_v6_address : Faker::Internet.public_ip_v4_address,
-    user_agent: Faker::Internet.user_agent,
-    country: rand(6).zero? ? ENUMS::COUNTRIES["United States"] : ENUMS::COUNTRIES.keys.sample,
-    postal_code: nil,
-    latitude: nil,
-    longitude: nil,
-    payable: rand(10).zero? ? false : true,
-    reason: nil,
-    displayed_at: displayed_at,
-    displayed_at_date: displayed_at.to_date,
-    clicked_at: rand(100) <= 2 ? displayed_at : nil,
-    fallback_campaign: campaign.fallback,
-  }
-end
-
-def build_impressions(displayed_at, max_impressions_per_second: 25)
-  rand(max_impressions_per_second + 1).times.map {
-    build_impression displayed_at
-  }.compact
-end
-
-def build_impressions_for_minute(time)
-  displayed_at = Time.new(time.year, time.month, time.day, time.hour, time.min, 0, 0)
-  impressions = []
-  while displayed_at.min == time.min && displayed_at.sec <= 59
-    impressions.concat build_impressions(displayed_at)
-    displayed_at = displayed_at.advance(seconds: rand(11))
+class ImpressionSeeder
+  def self.run(max_impressions)
+    new(max_impressions).call
   end
-  impressions
-end
 
-def build_impressions_for_hour(time)
-  displayed_at = Time.new(time.year, time.month, time.day, time.hour, 0, 0, 0)
-  impressions = []
-  while displayed_at.hour == time.hour && displayed_at.min <= 59
-    impressions.concat build_impressions_for_minute(displayed_at)
-    displayed_at = displayed_at.advance(minutes: rand(3))
+  def initialize(max_impressions)
+    @publisher = User.publisher.first
+    @campaign_cache = {}
+    @max_impressions_per_second = (max_impressions || 1_000) / (60 * 60 * 24 * 30 * 2)
   end
-  impressions
-end
 
-def build_impressions_for_day(date)
-  displayed_at = Time.new(date.year, date.month, date.day, 0, 0, 0, 0)
-  impressions = []
-  while displayed_at.day == date.day && displayed_at.hour <= 23
-    impressions.concat build_impressions_for_hour(displayed_at)
-    displayed_at = displayed_at.advance(hours: 1)
+  def call
+    # This will create 30M impressions (give or take) when @max_impressions_per_second is set to 6
+    # Currently configured to use 4 CPU cores... customize for the number of cores on your machine
+    # NOTE: this will peg your hardware for a while
+    print "[SEEDING DATABASE]: Seeding impressions"
+    pids = [
+      Process.fork { sleep 0; create_impressions_for_month "2019-01-01" },
+      Process.fork { sleep 30; create_impressions_for_month "2019-02-01" },
+      Process.fork { sleep 60; create_impressions_for_month "2019-03-01" },
+      Process.fork { sleep 90; create_impressions_for_month "2019-04-01" },
+    ]
+    pids.each { |pid| Process.waitpid pid }
+    puts
   end
-  impressions
-end
 
-def create_impressions_for_month(date_string)
-  start_date = Date.parse(date_string).beginning_of_month
-  return if Impression.where(displayed_at: start_date).count > 0
-  partition_table_name = "impressions_#{start_date.to_s "yyyy_mm"}"
+  private
 
-  current_date = start_date
-  while current_date <= start_date.end_of_month
-    csv_path = "/tmp/impressions-#{current_date.iso8601}.csv"
-    puts "Creating #{csv_path}"
-    list = build_impressions_for_day(current_date.to_time)
-    CSV.open(csv_path, "wb") do |csv|
-      list.each do |record|
-        csv << record.values
+  def build_impression(displayed_at)
+    property = @publisher.properties.sample
+    campaign = @campaign_cache[property.id] || Campaign.property(property).limit(1).first
+    @campaign_cache[property.id] = campaign
+    return nil unless campaign
+    {
+      id: SecureRandom.uuid,
+      campaign_id: campaign.id,
+      property_id: property.id,
+      ip: rand(6).zero? ? Faker::Internet.ip_v6_address : Faker::Internet.public_ip_v4_address,
+      user_agent: Faker::Internet.user_agent,
+      country: rand(6).zero? ? ENUMS::COUNTRIES["United States"] : ENUMS::COUNTRIES.keys.sample,
+      postal_code: nil,
+      latitude: nil,
+      longitude: nil,
+      payable: rand(10).zero? ? false : true,
+      reason: nil,
+      displayed_at: displayed_at,
+      displayed_at_date: displayed_at.to_date,
+      clicked_at: rand(100) <= 2 ? displayed_at : nil,
+      fallback_campaign: campaign.fallback,
+    }
+  end
+
+  def build_impressions(displayed_at)
+    rand(@max_impressions_per_second + 1).times.map {
+      build_impression displayed_at
+    }.compact
+  end
+
+  def build_impressions_for_minute(time)
+    displayed_at = Time.new(time.year, time.month, time.day, time.hour, time.min, 0, 0)
+    impressions = []
+    while displayed_at.min == time.min && displayed_at.sec <= 59
+      impressions.concat build_impressions(displayed_at)
+      displayed_at = displayed_at.advance(seconds: rand(11))
+    end
+    impressions
+  end
+
+  def build_impressions_for_hour(time)
+    displayed_at = Time.new(time.year, time.month, time.day, time.hour, 0, 0, 0)
+    impressions = []
+    while displayed_at.hour == time.hour && displayed_at.min <= 59
+      impressions.concat build_impressions_for_minute(displayed_at)
+      displayed_at = displayed_at.advance(minutes: rand(3))
+    end
+    impressions
+  end
+
+  def build_impressions_for_day(date)
+    displayed_at = Time.new(date.year, date.month, date.day, 0, 0, 0, 0)
+    impressions = []
+    while displayed_at.day == date.day && displayed_at.hour <= 23
+      impressions.concat build_impressions_for_hour(displayed_at)
+      displayed_at = displayed_at.advance(hours: 1)
+    end
+    impressions
+  end
+
+  def create_impressions_for_month(date_string)
+    start_date = Date.parse(date_string).beginning_of_month
+    return if Impression.where(displayed_at: start_date).count > 0
+    partition_table_name = "impressions_#{start_date.to_s "yyyy_mm"}"
+
+    current_date = start_date
+    while current_date <= start_date.end_of_month
+      csv_path = "/tmp/impressions-#{current_date.iso8601}.csv"
+      puts "Creating #{csv_path}"
+      list = build_impressions_for_day(current_date.to_time)
+      CSV.open(csv_path, "wb") do |csv|
+        list.each do |record|
+          csv << record.values
+        end
       end
-    end
 
-    begin
-      puts "Copying #{csv_path} with #{list.length} rows to Postgres"
-      ActiveRecord::Base.connection.execute "COPY \"#{partition_table_name}\" FROM '#{csv_path}' CSV;"
-    rescue => e
-      puts "Failed to copy #{csv_path} to Postgres! #{e}"
-    ensure
-      FileUtils.rm_f csv_path, verbose: true
-    end
+      begin
+        puts "Copying #{csv_path} with #{list.length} rows to Postgres"
+        ActiveRecord::Base.connection.execute "COPY \"#{partition_table_name}\" FROM '#{csv_path}' CSV;"
+      rescue => e
+        puts "Failed to copy #{csv_path} to Postgres! #{e}"
+      ensure
+        FileUtils.rm_f csv_path, verbose: true
+      end
 
-    current_date = current_date.advance(days: 1)
+      current_date = current_date.advance(days: 1)
+    end
   end
 end
-
-# This will create 30M impressions (give or take) when max_impressions_per_second is set to 6
-# Currently configured to use 4 CPU cores... customize for the number of cores on your machine
-# NOTE: this will peg your hardware for a while
-# pids = [
-#   Process.fork { sleep 0; create_impressions_for_month "2019-01-01" },
-#   Process.fork { sleep 30; create_impressions_for_month "2019-02-01" },
-#   Process.fork { sleep 60; create_impressions_for_month "2019-03-01" },
-#   Process.fork { sleep 90; create_impressions_for_month "2019-04-01" },
-# ]
-# pids.each { |pid| Process.waitpid pid }
-
-create_impressions_for_month "2019-10-01"
