f72d8f9b3af9caae292b6b4c71d896bdbf3d5f3d
Eric Berry <coderberry@gmail.com>
2018-12-06 16:32:44 -0700

    Revert "Update fixture"
    This reverts commit 82ce10ad7fdabfbb3c75ac31e6aa675bbf245691.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,8 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get events_index_url
     assert_response :success
   end
+
 end
