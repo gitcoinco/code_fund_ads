76b435a08767aa6a70d407cbdf092f12f66c78bb
Eric Berry <coderberry@gmail.com>
2018-12-06 15:46:35 -0700

    Update fixture

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,9 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get events_index_url
     assert_response :success
   end
-
 end
