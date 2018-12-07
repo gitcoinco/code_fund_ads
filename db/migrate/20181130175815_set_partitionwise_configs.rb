3e6118bd4e08636acffe388c3128573ee3a211a0
Nathan Hopkins <natehop@gmail.com>
2018-12-07 14:09:55 -0700

    Update migration for settings per db not system

--- db/migrate/20181130175815_set_partitionwise_configs.rb
+++ db/migrate/20181130175815_set_partitionwise_configs.rb
@@ -1,15 +1,13 @@
 class SetPartitionwiseConfigs < ActiveRecord::Migration[5.2]
-  # disable_ddl_transaction!
+  disable_ddl_transaction!
 
   def up
-    # Disabled until Heroku supports this
-    # execute "ALTER SYSTEM SET enable_partitionwise_join=on;"
-    # execute "ALTER SYSTEM SET enable_partitionwise_aggregate=on;"
+    execute "SET enable_partitionwise_join=on;"
+    execute "SET enable_partitionwise_aggregate=on;"
   end
 
   def down
-    # Disabled until Heroku supports this
-    # execute "ALTER SYSTEM RESET enable_partitionwise_join;"
-    # execute "ALTER SYSTEM RESET enable_partitionwise_aggregate;"
+    execute "RESET enable_partitionwise_join;"
+    execute "RESET enable_partitionwise_aggregate;"
   end
 end
