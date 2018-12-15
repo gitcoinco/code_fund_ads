a85f6cc9cb6374be9962984b333d2a5f72f23c16
Nathan Hopkins <natehop@gmail.com>
2018-12-14 19:07:14 -0700

    Safer Rails increment (#125)

--- app/jobs/increment_impressions_count_cache_job.rb
+++ app/jobs/increment_impressions_count_cache_job.rb
@@ -3,17 +3,24 @@ class IncrementImpressionsCountCacheJob < ApplicationJob
 
   # TODO: add protections to guard against multiple counts if errros occur
   def perform(impression)
-    increment impression.campaign.total_impressions_count_cache_key
-    increment impression.campaign.daily_impressions_count_cache_key(Date.current)
+    Rails.cache.write(
+      impression.campaign.total_impressions_count_cache_key,
+      impression.campaign.total_impressions_count.to_i + 1
+    )
 
-    increment impression.property.total_impressions_count_cache_key
-    increment impression.property.daily_impressions_count_cache_key(Date.current)
-  end
+    Rails.cache.write(
+      impression.campaign.daily_impressions_count_cache_key(Date.current),
+      impression.campaign.daily_impressions_count(Date.current).to_i + 1
+    )
 
-  private
+    Rails.cache.write(
+      impression.property.total_impressions_count_cache_key,
+      impression.property.total_impressions_count.to_i + 1
+    )
 
-  def increment(cache_key)
-    return Rails.cache.write(cache_key, 1) unless Rails.cache.exist?(cache_key)
-    Rails.cache.increment cache_key
+    Rails.cache.write(
+      impression.property.daily_impressions_count_cache_key(Date.current),
+      impression.property.daily_impressions_count(Date.current).to_i + 1
+    )
   end
 end
