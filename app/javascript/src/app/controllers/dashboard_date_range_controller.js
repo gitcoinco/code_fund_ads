51abfef7d7e532056fdc026afd7b2a2d8adf3c43
Eric Berry <coderberry@gmail.com>
2018-12-14 17:23:41 -0700

    Small fixes with SVG and utc date selection (#119)

--- app/javascript/src/app/controllers/dashboard_date_range_controller.js
+++ app/javascript/src/app/controllers/dashboard_date_range_controller.js
@@ -2,7 +2,7 @@
 // Our use of it here is simply because its already a dependency
 // The verbose use of the `jQuery` variable instead of `$` is intentional so its use is easier to identify
 import { Controller } from 'stimulus';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 export default class extends Controller {
   static targets = ['form', 'datePicker'];
@@ -15,26 +15,35 @@ export default class extends Controller {
     jQuery(this.datePickerTarget)
       .daterangepicker({
         opens: 'left',
-        maxDate: moment(),
+        maxDate: moment().utc(),
         maxSpan: {
           months: 1,
         },
         ranges: {
-          'This Week': [moment().startOf('week'), moment()],
+          'This Week': [
+            moment()
+              .utc()
+              .startOf('week'),
+            moment(),
+          ],
           'Last Week': [
             moment()
+              .utc()
               .add(-1, 'week')
               .startOf('week'),
             moment()
+              .utc()
               .add(-1, 'week')
               .endOf('week'),
           ],
           'This Month': [moment().startOf('month'), moment()],
           'Last Month': [
             moment()
+              .utc()
               .add(-1, 'month')
               .startOf('month'),
             moment()
+              .utc()
               .add(-1, 'month')
               .endOf('month'),
           ],
