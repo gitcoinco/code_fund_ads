18268a7f61c06b4f393eecc03c02f06212219eba
Nathan Hopkins <natehop@gmail.com>
2018-12-14 18:38:38 -0700

    Increment in a Rails friendly way (#123)

--- app/jobs/increment_clicks_count_cache_job.rb
+++ app/jobs/increment_clicks_count_cache_job.rb
@@ -3,10 +3,17 @@ class IncrementClicksCountCacheJob < ApplicationJob
 
   # TODO: add protections to guard against multiple counts if errros occur
   def perform(impression)
-    Rails.cache.redis.incr impression.campaign.total_clicks_count_cache_key
-    Rails.cache.redis.incr impression.campaign.daily_clicks_count_cache_key(Date.current)
+    increment impression.campaign.total_clicks_count_cache_key
+    increment impression.campaign.daily_clicks_count_cache_key(Date.current)
 
-    Rails.cache.redis.incr impression.property.total_clicks_count_cache_key
-    Rails.cache.redis.incr impression.property.daily_clicks_count_cache_key(Date.current)
+    increment impression.property.total_clicks_count_cache_key
+    increment impression.property.daily_clicks_count_cache_key(Date.current)
+  end
+
+  private
+
+  def increment(cache_key)
+    return Rails.cache.write(cache_key, 1) unless Rails.cache.exist?(cache_key)
+    Rails.cache.increment cache_key
   end
 end
