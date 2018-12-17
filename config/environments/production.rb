a036515663b5adcf70941c6b6c7fb2f66bf62b09
Eric Berry <coderberry@gmail.com>
2018-12-17 06:56:55 -0700

    Set default mailer host to .app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: "codefund.io"}
+  config.action_mailer.default_url_options = {host: "codefund.app"}
 end
