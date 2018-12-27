e048ef9e8801fc8b0e2d99ed615ef4c7498db0b7
Nathan Hopkins <natehop@gmail.com>
2018-12-27 08:30:45 -0700

    Fix issue with sparklines (#164)

--- app/models/concerns/sparklineable.rb
+++ app/models/concerns/sparklineable.rb
@@ -2,8 +2,8 @@ module Sparklineable
   extend ActiveSupport::Concern
 
   def sparkline_impressions(start_date, end_date)
-    date_range = (start_date...end_date).to_a
-    daily_impressions_counts(@start_date, @end_date).map.with_index do |count, i|
+    date_range = (start_date..end_date).to_a
+    daily_impressions_counts(start_date, end_date).map.with_index do |count, i|
       {
         name: "Impressions",
         date: date_range[i].to_s("%F"),
