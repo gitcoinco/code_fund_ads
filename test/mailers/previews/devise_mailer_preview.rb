a4a6b88e1b0d2219568174a52ee0639b065e7696
Eric Berry <coderberry@gmail.com>
2018-12-29 07:39:17 -0700

    Add instructions to invitation emails (#177)

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -1,31 +1,31 @@
 # Preview all emails at http://localhost:3000/rails/mailers/applicant_mailer
 class DeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
-    user = User.new(email: "example@codefund.io", first_name: "Joe")
+    user = User.new(email: "example@codefund.io", first_name: "Joe", roles: ["publisher"])
     DeviseMailer.confirmation_instructions(user, SecureRandom.uuid)
   end
 
   def reset_password_instructions
-    user = User.new(email: "example@codefund.io", first_name: "Joe")
+    user = User.new(email: "example@codefund.io", first_name: "Joe", roles: ["publisher"])
     DeviseMailer.reset_password_instructions(user, SecureRandom.uuid)
   end
 
   def unlock_instructions
-    user = User.new(email: "example@codefund.io", first_name: "Joe")
+    user = User.new(email: "example@codefund.io", first_name: "Joe", roles: ["publisher"])
     DeviseMailer.unlock_instructions(user, SecureRandom.uuid)
   end
 
   def email_changed
-    user = User.new(email: "example@codefund.io", first_name: "Joe")
+    user = User.new(email: "example@codefund.io", first_name: "Joe", roles: ["publisher"])
     DeviseMailer.email_changed(user)
   end
 
   def password_change
-    user = User.new(email: "example@codefund.io", first_name: "Joe")
+    user = User.new(email: "example@codefund.io", first_name: "Joe", roles: ["publisher"])
     DeviseMailer.password_change(user)
   end
 
-  def invitation_instructions
+  def publisher_invitation_instructions
     user = User.new(
       email: "example@codefund.io",
       first_name: "Joe",
@@ -33,7 +33,22 @@ class DeviseMailerPreview < ActionMailer::Preview
       invitation_created_at: Time.current,
       invitation_limit: 0,
       invited_by_type: "User",
-      invited_by_id: 1
+      invited_by_id: 1,
+      roles: ["publisher"]
+    )
+    DeviseMailer.invitation_instructions(user, SecureRandom.uuid)
+  end
+
+  def advertiser_invitation_instructions
+    user = User.new(
+      email: "example@codefund.io",
+      first_name: "Joe",
+      invitation_token: SecureRandom.uuid,
+      invitation_created_at: Time.current,
+      invitation_limit: 0,
+      invited_by_type: "User",
+      invited_by_id: 1,
+      roles: ["advertiser"]
     )
     DeviseMailer.invitation_instructions(user, SecureRandom.uuid)
   end
