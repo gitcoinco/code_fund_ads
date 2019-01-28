15e93e428e9961378d1729d79771f0cab56642ad
Nathan Hopkins <natehop@gmail.com>
2019-01-28 12:05:59 -0700

    Fix for multiple select on job form

--- config/initializers/feature_flag.rb
+++ config/initializers/feature_flag.rb
@@ -1,6 +1,6 @@
 class FeatureFlag
   def self.enabled?(feature, user = nil)
-    @env_enabled ||= ENV["ENABLED_FEATURES"].split(",").map(&:to_sym)
+    @env_enabled ||= ENV["ENABLED_FEATURES"].to_s.split(",").map(&:to_sym)
     return true if @env_enabled.include?(feature)
     return true if user&.administrator?
     false
