c821f4353d63261fecd958e815d52e36b4608d5d
Nathan Hopkins <natehop@gmail.com>
2019-02-26 14:57:59 -0700

    Honor selected dates on campaign dashboard (#353)
    * Honor selected dates on campaign dashboard
    
    * Add some numbers to the admin page

--- app/controllers/administrator_dashboards_controller.rb
+++ app/controllers/administrator_dashboards_controller.rb
@@ -2,16 +2,7 @@ class AdministratorDashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @active_campaigns = Campaign.active.order(name: :asc).limit(9)
-    @impressions_count = Impression.between(@start_date, @end_date).count
-    @clicks_count = Impression.clicked.between(@start_date, @end_date).count
-    @average_click_rate = if @impressions_count > 0
-      (@clicks_count / @impressions_count.to_f) * 100
-    else
-      0.0
-    end
-    @total_revenue = Money.new 0, "USD"
-    @total_distributions = Money.new 0, "USD"
-    @total_profit = Money.new 0, "USD"
+    @active_campaigns = Campaign.active.premium.order(name: :asc)
+    @impressions = Impression.between(@start_date, @end_date)
   end
 end
--- app/helpers/impressions_helper.rb
+++ app/helpers/impressions_helper.rb
@@ -1,2 +1,6 @@
 module ImpressionsHelper
+  def click_rate(impressions_relation)
+    return 0.0 unless impressions_relation.count > 0
+    (impressions_relation.clicked.count / impressions_relation.count.to_f) * 100
+  end
 end
