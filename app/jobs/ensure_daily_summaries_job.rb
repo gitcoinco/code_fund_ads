f558f14969b60e0d19dbaf3e2f977ef837ad3397
Nathan Hopkins <natehop@gmail.com>
2019-03-26 09:19:23 -0600

    Update ensure daily summaries to include archived campaigns
    Also find in batches to support network growth.

--- app/jobs/ensure_daily_summaries_job.rb
+++ app/jobs/ensure_daily_summaries_job.rb
@@ -8,15 +8,20 @@ class EnsureDailySummariesJob < ApplicationJob
       Date.current.beginning_of_month.advance(months: -2),
     ]
 
-    Campaign.active.each do |campaign|
-      dates.each do |date|
-        campaign.daily_impressions_counts(date, date.end_of_month)
+    Campaign.in_batches.each do |campaigns|
+      campaigns.each do |campaign|
+        dates.each do |date|
+          next unless campaign.available_on?(date)
+          campaign.daily_impressions_counts(date, date.end_of_month)
+        end
       end
     end
 
-    Property.active.each do |property|
-      dates.each do |date|
-        property.daily_impressions_counts(date, date.end_of_month)
+    Property.active.in_batches.each do |properties|
+      properties.each do |property|
+        dates.each do |date|
+          property.daily_impressions_counts(date, date.end_of_month)
+        end
       end
     end
   end
