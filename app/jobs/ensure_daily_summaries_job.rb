a54c5df79aed2a37860f1322dd1bfdaec0177913
Nathan Hopkins <natehop@gmail.com>
2019-04-22 10:46:11 -0600

    Simplify daily summaries job... and pick up all campaigns (#478)

--- app/jobs/ensure_daily_summaries_job.rb
+++ app/jobs/ensure_daily_summaries_job.rb
@@ -2,26 +2,18 @@ class EnsureDailySummariesJob < ApplicationJob
   queue_as :low
 
   def perform
-    dates = [
-      Date.current.beginning_of_month.advance(months: -2),
-      Date.current.beginning_of_month.advance(months: -1),
-      Date.current.beginning_of_month,
-    ]
+    start_date = Date.current.beginning_of_month.advance(months: -2)
+    end_date = Date.current.end_of_month
 
     Campaign.in_batches.each do |campaigns|
       campaigns.each do |campaign|
-        dates.each do |date|
-          next unless campaign.available_on?(date)
-          campaign.daily_impressions_counts(date, date.end_of_month, fresh: true)
-        end
+        CreateDailySummariesJob.perform_later campaign, start_date.iso8601, end_date.iso8601, nil
       end
     end
 
     Property.in_batches.each do |properties|
       properties.each do |property|
-        dates.each do |date|
-          property.daily_impressions_counts(date, date.end_of_month, fresh: true)
-        end
+        CreateDailySummariesJob.perform_later property, start_date.iso8601, end_date.iso8601, nil
       end
     end
   end
