07619dd3dfc5229e25ef684e604cff97a25169a8
Eric Berry <coderberry@gmail.com>
2019-05-30 12:07:01 -0600

    Update creative_image_analyzer.rb

--- app/analyzers/creative_image_analyzer.rb
+++ app/analyzers/creative_image_analyzer.rb
@@ -4,6 +4,7 @@ class CreativeImageAnalyzer < ActiveStorage::Analyzer::ImageAnalyzer
     md[:name] = @blob.filename
     return md unless md[:height].present? && md[:width].present?
 
+    md[:format] = "icon"  if md[:width].to_i == 20  && md[:height] == 20
     md[:format] = "icon"  if md[:width].to_i == 40  && md[:height] == 40
     md[:format] = "small" if md[:width].to_i == 200 && md[:height] == 200
     md[:format] = "large" if md[:width].to_i == 260 && md[:height] == 200
