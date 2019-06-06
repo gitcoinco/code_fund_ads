c3a5915103e55b9f67ba577f027f16f256968b26
Nathan Hopkins <natehop@gmail.com>
2019-06-06 12:42:50 -0600

    Don't enqueue noops (#522)
    * Don't enqueue noops
    
    * Fix scope call

--- app/jobs/ensure_daily_summaries_job.rb
+++ app/jobs/ensure_daily_summaries_job.rb
@@ -7,12 +7,14 @@ class EnsureDailySummariesJob < ApplicationJob
 
     Campaign.in_batches.each do |campaigns|
       campaigns.each do |campaign|
+        next unless Impression.between(start_date, end_date).where(campaign: campaign).exists?
         CreateDailySummariesJob.perform_later campaign, start_date.iso8601, end_date.iso8601, nil
       end
     end
 
     Property.in_batches.each do |properties|
       properties.each do |property|
+        next unless Impression.between(start_date, end_date).where(property: property).exists?
         CreateDailySummariesJob.perform_later property, start_date.iso8601, end_date.iso8601, nil
       end
     end
--- app/jobs/ensure_scoped_daily_summaries_job.rb
+++ app/jobs/ensure_scoped_daily_summaries_job.rb
@@ -7,8 +7,10 @@ class EnsureScopedDailySummariesJob < ApplicationJob
 
     Campaign.in_batches.each do |campaigns|
       campaigns.each do |campaign|
+        next unless Impression.between(start_date, end_date).where(campaign: campaign).exists?
         Property.in_batches.each do |properties|
           properties.each do |property|
+            next unless Impression.between(start_date..end_date).where(campaign: campaign, property: property).exists?
             CreateDailySummariesJob.perform_later campaign, start_date.iso8601, end_date.iso8601, property
           end
         end
@@ -17,8 +19,10 @@ class EnsureScopedDailySummariesJob < ApplicationJob
 
     Property.in_batches.each do |properties|
       properties.each do |property|
+        next unless Impression.between(start_date, end_date).where(property: property).exists?
         Campaign.in_batches.each do |campaigns|
           campaigns.each do |campaign|
+            next unless Impression.between(start_date..end_date).where(property: property, campaign: campaign).exists?
             CreateDailySummariesJob.perform_later property, start_date.iso8601, end_date.iso8601, campaign
           end
         end
