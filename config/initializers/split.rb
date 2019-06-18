b8deda18a255c7ea1138805ae1fca99b6986a8f8
Nathan Hopkins <natehop@gmail.com>
2019-06-18 16:20:47 -0600

    Update split redis config

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,4 +1,11 @@
 Split.configure do |config|
   config.algorithm = Split::Algorithms::Whiplash # multi-armed bandit
   config.winning_alternative_recalculation_interval = 3600 # 1 hour
+  config.persistence = Split::Persistence::RedisAdapter.with_config(
+    lookup_by: ->(context) { context&.session&.id },
+    namespace: "splitrb",
+    expire_seconds: 30,
+  )
 end
+
+Split.redis = Rails.cache.redis
