77e3ec9f78889ab5d8806330b364587410f6dec9
Nathan Hopkins <natehop@gmail.com>
2019-06-19 15:11:12 -0600

    More split yak shaving

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -2,7 +2,7 @@ Split.configure do |config|
   config.algorithm = Split::Algorithms::Whiplash # multi-armed bandit
   config.winning_alternative_recalculation_interval = 3600 # 1 hour
   config.persistence = :cookie
-  config.persistence_cookie_length = 0
+  config.persistence_cookie_length = 2.hours.seconds.to_i
 end
 
 Split.redis = Rails.cache.redis
