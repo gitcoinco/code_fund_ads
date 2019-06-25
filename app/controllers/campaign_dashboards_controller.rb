53227f99afda84b9c017ac1ce47047c362807d9b
Eric Berry <coderberry@gmail.com>
2019-06-25 11:00:08 -0600

    Add dashboard for campaigns (#555)

--- app/controllers/campaign_dashboards_controller.rb
+++ app/controllers/campaign_dashboards_controller.rb
@@ -2,6 +2,20 @@ class CampaignDashboardsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_campaign
 
+  def show
+    payload = {
+      resource: {dashboard: ENV["METABASE_CAMPAIGN_DASHBOARD_ID"].to_i},
+      params: {
+        "campaign_id" => @campaign.id,
+        "start_date" => @start_date.strftime("%F"),
+        "end_date" => @end_date.strftime("%F"),
+      },
+    }
+    token = JWT.encode payload, ENV["METABASE_SECRET_KEY"]
+
+    @iframe_url = ENV["METABASE_SITE_URL"] + "/embed/dashboard/" + token + "#bordered=false&titled=false"
+  end
+
   private
 
   def set_campaign
