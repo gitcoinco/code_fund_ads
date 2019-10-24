e1785b7257b020a9df26e2f3a71d3dacd4f54696
Curtis Mckee <cmckee.dev@gmail.com>
2019-10-24 14:03:06 -0600

    Properties#Show - Instructions tab redesign (#721)

--- app/javascript/themes/redesign/theme.js
+++ app/javascript/themes/redesign/theme.js
@@ -8,6 +8,10 @@ import 'jquery-validation'
 // JS
 import 'themes/redesign/javascript/theme.min'
 import 'themes/redesign/vendor/easy-pie-chart/jquery.easypiechart.min.js'
+
+// TODO: Remove the hs.core and hs.clipboard in favor of stimulus
+import 'themes/redesign/javascript/hs.core'
+import 'themes/redesign/javascript/components/hs.clipboard'
 // Images
 import 'themes/redesign/images/logo/twitter.svg'
 import 'themes/redesign/images/decoration/bubble4.svg'
