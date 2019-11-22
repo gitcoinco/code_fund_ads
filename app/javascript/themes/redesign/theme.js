3e7395ce36b04a6a2ae695e960b0a2a45af6ba97
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-22 14:50:11 -0500

    fix compilation error (#828)
    This will fix the asset compilation and it also removes some unneeded images that were added in #826 or that we just aren't using.

--- app/javascript/themes/redesign/theme.js
+++ app/javascript/themes/redesign/theme.js
@@ -7,9 +7,6 @@ import 'jquery-validation'
 // TODO: Remove the hs.core and hs.clipboard in favor of stimulus
 import 'themes/redesign/javascript/hs.core'
 import 'themes/redesign/javascript/components/hs.clipboard'
-// Images
-import 'themes/redesign/images/logo/twitter.svg'
-import 'themes/redesign/images/decoration/bubble4.svg'
 
 document.addEventListener('turbolinks:before-cache', function () {
   $('[data-toggle="tooltip"]').tooltip('hide')
