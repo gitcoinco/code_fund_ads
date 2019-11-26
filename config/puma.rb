67aca7c8cbf9bce26f6587acd1b1ef6450762ac5
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-26 11:20:15 -0500

    allow heroku to receive puma pool usage data (#836)
    Add barnes start command to puma config file based on code triage setup. This will allow Heroku to receive Puma Pool Usage Data.

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,4 @@
+require "barnes"
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -36,6 +37,10 @@ workers ENV.fetch("WEB_CONCURRENCY", 4).to_i
 
 on_worker_fork do
   FileUtils.touch "/tmp/app-initialized"
+end
+
+on_worker_boot do
+  Barnes.start
 end
 
 # Allow puma to be restarted by `rails restart` command.
