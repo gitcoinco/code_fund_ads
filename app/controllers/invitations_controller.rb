3024a5ed37de000daac719fd5da63cc483395a14
Andrew Mason <andrewmcodes@protonmail.com>
2019-12-19 15:29:00 -0500

    fix layout for admin invitations controller (#956)
    Fixes #954

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,6 +7,11 @@ class InvitationsController < Devise::InvitationsController
     CreateSlackNotificationJob.perform_later text: ":email: #{resource.email} just registered via invitation" if resource.errors.empty?
   end
 
+  def new
+    self.resource = resource_class.new
+    render :new, layout: "application"
+  end
+
   def after_accept_path_for(user)
     helpers.default_dashboard_path user
   end
