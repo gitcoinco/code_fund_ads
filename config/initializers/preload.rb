b965167aa803afefe1443a0715a714446a015fa1
Nathan Hopkins <natehop@gmail.com>
2020-01-14 19:19:38 -0700

    Yak shaving to make webpack happy

--- config/initializers/preload.rb
+++ config/initializers/preload.rb
@@ -1,2 +1,10 @@
-Mmdb.mmdb
-Country.all
+begin
+  Mmdb.mmdb
+rescue => e
+  Rails.logger.error "Failed to preload MaxMindDB data! #{e}"
+end
+begin
+  Country.all
+rescue
+  Rails.logger.error "Failed to preload Countries GEM data! #{e}"
+end
