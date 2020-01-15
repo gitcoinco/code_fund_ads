5b3e752284adac8d429e2373f8f6f7c071e83cea
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-15 18:39:37 -0500

    specifically require rails libraries (#1013)
    only require Rails libraries that we actually use, this shaves off some memory. See https://github.com/rails/rails/blob/v6.0.2.1/railties/lib/rails/all.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative "boot"
 
-require "rails/all"
+# only require Rails libraries that we actually use, this shaves off some memory
+# ActionMailbox and ActionText are not currently used by the app
+# see https://github.com/rails/rails/blob/v6.0.2.1/railties/lib/rails/all.rb
+%w[
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |lib|
+  require lib
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
