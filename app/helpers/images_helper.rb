61cc7d53daa14cba0dccb9051468d3a06f914703
Andrew Mason <andrewmcodes@protonmail.com>
2020-01-27 19:22:15 -0500

    update images overview to show some more information on the images (#1046)
    fixes #1042

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,5 @@
 module ImagesHelper
+  def image_dimensions(image)
+    "#{number_with_precision(image.blob.metadata[:width], precision: 0)}x#{number_with_precision(image.blob.metadata[:height], precision: 0)}"
+  end
 end
