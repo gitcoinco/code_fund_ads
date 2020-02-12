dfbd35bdb9517c8e1543f453f7bf80f569a6d805
Nathan Hopkins <natehop@gmail.com>
2020-02-12 06:53:38 -0700

    Give action cable thread pool its own env var

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -1,2 +1,2 @@
-Rails.application.config.action_cable.worker_pool_size = ENV.fetch("RAILS_MAX_THREADS", 10).to_i
+Rails.application.config.action_cable.worker_pool_size = ENV.fetch("ACTION_CABLE_MAX_THREADS", 10).to_i
 ActionCable.server.config.logger = Logger.new(nil) if ENV["SILENCE_ACTION_CABLE"] == "true"
