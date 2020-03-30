57b6c693420199f7ffd9c28b649c4bb6a2b1b2ec
Hopsoft <natehop@gmail.com>
2020-03-30 15:27:50 -0600

    Auto reset bundle on HTTP new (#1186)

--- app/controllers/concerns/campaign_bundles/stashable.rb
+++ app/controllers/concerns/campaign_bundles/stashable.rb
@@ -7,6 +7,7 @@ module CampaignBundles
     end
 
     def stashed_campaign_bundle
+      session[:stashed_campaign_bundle] = nil if !@stimulus_reflex && try(:action_name) == "new"
       CampaignBundle.new(stashed_campaign_bundle_params).tap do |campaign_bundle|
         campaign_bundle.start_date ||= Date.current
         campaign_bundle.end_date ||= 30.days.from_now.to_date
