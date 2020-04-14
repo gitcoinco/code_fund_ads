f72c2f3edf6d1c313df1f36626cc7503c98a3ba4
Eric Berry <coderberry@gmail.com>
2020-04-14 10:50:24 -0600

    Use Metabase Admin Dashboard on admin dashboard page (#1222)
    * Use Metabase Admin Dashboard on admin dashboard page
    
    * StandardRb

--- app/controllers/administrator_dashboards_controller.rb
+++ app/controllers/administrator_dashboards_controller.rb
@@ -1,29 +1,14 @@
 class AdministratorDashboardsController < ApplicationController
-  include Sortable
-
   before_action :authenticate_user!
   before_action :authenticate_administrator!
 
-  set_default_sorted_by :end_date
-  set_default_sorted_direction :desc
-
   def show
-    campaigns = Campaign.active.premium.includes(:organization).order(order_by)
-    @pagy, @campaigns = pagy(campaigns, page: @page)
-  end
+    payload = {
+      resource: {dashboard: ENV["METABASE_ADMINISTRATOR_DASHBOARD_ID"].to_i},
+      params: {}
+    }
+    token = JWT.encode payload, ENV["METABASE_SECRET_KEY"]
 
-  protected
-
-  def set_sortable_columns
-    @sortable_columns ||= %w[
-      start_date
-      end_date
-      name
-      updated_at
-      created_at
-      hourly_budget_cents
-      daily_budget_cents
-      total_budget_cents
-    ]
+    @iframe_url = ENV["METABASE_SITE_URL"] + "/embed/dashboard/" + token + "#bordered=false&titled=false"
   end
 end
