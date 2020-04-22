gitcoinco/code_fund_ads

commit ca057eac9f9c2b0897b51665bcc1dafa26fe77d3

add missing validations for creative form (#1227)
It is currently possible to submit the creative form without images and with copy text that exceeds 85 characters in length. This commit adds enhanced validations around creatives.

app/models/creative.rb
@@ -23,6 +23,8 @@
 #
 
 class Creative < ApplicationRecord
+  MAX_COPY_LENGTH = 85
+
   # extends ...................................................................
   # includes ..................................................................
   include Eventable
@@ -46,8 +48,12 @@ class Creative < ApplicationRecord
   validates :name, length: {maximum: 255, allow_blank: false}
   validates :status, inclusion: {in: ENUMS::CREATIVE_STATUSES.values}
   validates :creative_type, inclusion: {in: ENUMS::CREATIVE_TYPES.values}
-  validate :validate_images
+  validate :validate_icon_image
+  validate :validate_small_image
+  validate :validate_wide_image
+  validate :validate_large_image
   validate :validate_status_change
+  validate :validate_copy_length
 
   # callbacks .................................................................
   after_commit :touch_campaigns, on: [:update]
@@ -191,10 +197,29 @@ class Creative < ApplicationRecord
     end
   end
 
-  def validate_images
-    if standard_images.exists? && sponsor_image
-      errors.add :images, "cannot include both standard and sponsor types"
-    end
+  def validate_icon_image
+    return if icon_image
+    errors.add :base, "please select an icon image"
+  end
+
+  def validate_small_image
+    return if small_image
+    errors.add :base, "please select a small image"
+  end
+
+  def validate_wide_image
+    return if wide_image
+    errors.add :base, "please select a wide image"
+  end
+
+  def validate_large_image
+    return if large_image
+    errors.add :base, "please select a large image"
+  end
+
+  def validate_copy_length
+    return unless body.length + cta.length + headline.length > MAX_COPY_LENGTH
+    errors.add :base, "headline, body, and call to action cannot exceed a combined #{MAX_COPY_LENGTH} characters."
   end
 
   def validate_destroyable

test/controllers/advertisement_clicks_controller_test.rb
@@ -73,6 +73,7 @@ class AdvertisementClicksControllerTest < ActionDispatch::IntegrationTest
 
   test "advertisement click {{creative_name}} URL variable" do
     @campaign.update url: "https://example.com?custom={{creative_name}}"
+    attach_all_images!(creative: @creative, organization: @creative.organization)
     @creative.update name: "Ad Click: Creative Name > Test"
     get advertisement_clicks_url(@impression_id), params: @params, headers: @headers
     assert_response :redirect

test/integration/advertisements_test.rb
@@ -9,6 +9,8 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
     @premium_campaign.campaign_bundle.update_columns start_date: start_date, end_date: start_date.advance(months: 3)
     @premium_campaign.save!
     @premium_campaign.organization.update balance: Monetize.parse("$10,000 USD")
+    attach_all_images!(creative: @premium_campaign.creative, organization: @premium_campaign.organization)
+    @images = @premium_campaign.creative.images
     @property = amend(properties: :website, audience_id: @premium_campaign.audience_ids.sample)
     travel_to start_date.to_time.advance(days: 15)
   end
@@ -112,7 +114,7 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
   test "js: fallback campaign with matching keywords and country is displayed before generic fallback campaigns" do
     amend campaigns: :fallback, start_date: @premium_campaign.start_date, end_date: @premium_campaign.end_date
     copy campaigns: :fallback, keywords: @property.keywords.sample(2),
-         creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign").id]
+         creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign", images: @images).id]
     @premium_campaign.update_columns keywords: ["No Match"]
     get advertisements_path(@property, format: :js), headers: {"REMOTE_ADDR": ip_address("US")}
     assert Campaign.fallback.count == 2
@@ -167,7 +169,7 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
   # ----------------------------------------------------------------------------------------------------------
 
   test "js: property with assigner campaign will eventually display the assigner campaign" do
-    other_campaign = copy(campaigns: :premium, creative_ids: [copy(creatives: :premium).id])
+    other_campaign = copy(campaigns: :premium, creative_ids: [copy(creatives: :premium, images: @images).id])
     @premium_campaign.update_columns assigned_property_ids: [@property.id]
     @premium_campaign.creative.update body: "This is a premium campaign that has assigned the property"
     assert other_campaign.creative.body != @premium_campaign.creative.body
@@ -180,7 +182,7 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
   end
 
   test "js: property with assigner campaign will eventually display other matching campaigns" do
-    other_campaign = Campaign.create!(@premium_campaign.attributes.except("id").merge(creative_ids: [copy(creatives: :premium, body: "This is a non assigner premium campaign").id]))
+    other_campaign = Campaign.create!(@premium_campaign.attributes.except("id").merge(creative_ids: [copy(creatives: :premium, body: "This is a non assigner premium campaign", images: @images).id]))
     @premium_campaign.update_columns assigned_property_ids: [@property.id]
     @premium_campaign.creative.update_columns body: "This is a premium campaign that has assigned the property"
     assert other_campaign.creative.body != @premium_campaign.creative.body
@@ -193,7 +195,7 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
   end
 
   test "js: property with assigner campaign will never display other matching campaigns when restrict_to_assigner_campaigns is true" do
-    other_campaign = copy(campaigns: :premium, creative_ids: [copy(creatives: :premium).id])
+    other_campaign = copy(campaigns: :premium, creative_ids: [copy(creatives: :premium, images: @images).id])
     @premium_campaign.update_columns assigned_property_ids: [@property.id]
     @premium_campaign.creative.update_columns body: "This is a premium campaign that has assigned the property"
     @property.update_columns restrict_to_assigner_campaigns: true
@@ -224,7 +226,7 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
 
   test "js: property will show targeted premium campaign over a zero balance campaign with assigned property" do
     organization = copy(organizations: :default, balance: Money.new(0, "USD"))
-
+    attach_all_images!(creative: @premium_campaign.creative, organization: organization)
     user = copy users: :advertiser,
                 email: Faker::Internet.email,
                 password: "password",
@@ -233,7 +235,8 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
     creative = copy creatives: :premium,
                     organization: organization,
                     user: user,
-                    body: "This is an assigned premium campaign"
+                    body: "This is an assigned premium campaign",
+                    images: @images
 
     Campaign.create! @premium_campaign.attributes.except("id", "campaign_bundle_id").merge(
       organization: organization,
@@ -260,10 +263,10 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
           keywords: @property.keywords,
           start_date: @premium_campaign.start_date,
           end_date: @premium_campaign.end_date,
-          creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign").id]
+          creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign", images: @images).id]
 
     assigned = copy campaigns: :fallback, keywords: [],
-                    creative_ids: [copy(creatives: :fallback, body: "This is an assigned fallback campaign").id]
+                    creative_ids: [copy(creatives: :fallback, body: "This is an assigned fallback campaign", images: @images).id]
 
     @property.update_columns assigned_fallback_campaign_ids: [assigned.id]
 
@@ -281,14 +284,14 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
           keywords: @property.keywords,
           start_date: @premium_campaign.start_date,
           end_date: @premium_campaign.end_date,
-          creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign").id]
+          creative_ids: [copy(creatives: :fallback, body: "This is a targeted fallback campaign", images: @images).id]
 
     assigned = copy campaigns: :fallback, keywords: [],
-                    creative_ids: [copy(creatives: :fallback, body: "This is an assigned fallback campaign").id]
+                    creative_ids: [copy(creatives: :fallback, body: "This is an assigned fallback campaign", images: @images).id]
 
     assigned_and_targeted = copy campaigns: :fallback,
                                  keywords: @property.keywords,
-                                 creative_ids: [copy(creatives: :fallback, body: "This is an assigned and targeted fallback campaign").id]
+                                 creative_ids: [copy(creatives: :fallback, body: "This is an assigned and targeted fallback campaign", images: @images).id]
 
     @property.update_columns assigned_fallback_campaign_ids: [assigned.id, assigned_and_targeted.id]
 
@@ -301,12 +304,11 @@ class AdvertisementsTest < ActionDispatch::IntegrationTest
 
   test "js: fallback campaign with assigned property will not display on a different property even if the different property assigns the fallback campaign" do
     @premium_campaign.update_columns status: ENUMS::CAMPAIGN_STATUSES::PENDING
-
     fallback_campaign = amend campaigns: :fallback,
                               start_date: @premium_campaign.start_date,
                               end_date: @premium_campaign.end_date,
                               assigned_property_ids: [@property.id],
-                              creative: copy(creatives: :fallback, body: "This is a fallback campaign assigned to a different property")
+                              creative: copy(creatives: :fallback, body: "This is a fallback campaign assigned to a different property", images: @images)
 
     other_property = copy properties: :website, assigned_fallback_campaign_ids: [fallback_campaign.id]
 

test/test_helper.rb
@@ -48,6 +48,31 @@ class ActiveSupport::TestCase
     post user_session_url
   end
 
+  def attach_all_images!(creative:, organization:)
+    creative.add_image! attach_icon_image!(organization)
+    creative.add_image! attach_small_image!(organization)
+    creative.add_image! attach_large_image!(organization)
+    creative.add_image! attach_wide_image!(organization)
+  end
+
+  def attach_icon_image!(record)
+    name = "seed-20x20.png"
+    record.images.attach(
+      io: File.open(Rails.root.join("test/assets/images/seeds/seed-20x20.png")),
+      filename: "seed-20x20.png",
+      content_type: "image/png",
+      metadata: {
+        identified: true,
+        width: 20,
+        height: 20,
+        analyzed: true,
+        name: name,
+        format: ENUMS::IMAGE_FORMATS::ICON
+      }
+    )
+    record.images.search_metadata_name(name).metadata_format(ENUMS::IMAGE_FORMATS::ICON).order(created_at: :desc).first
+  end
+
   def attach_small_image!(record)
     name = "seed-200x200.png"
     record.images.attach(

commit aeb8db4ee391af2b0b21a28d102089cc3319de01

Fix creative validations (#1231)
Correctly validate that images have been uploaded with a creative due to the way we assign images after the object is created/updated.

I'm not overjoyed by moving the image validations farther from the database but I think this is the best solution given the way we assign images.

Also adds a few enhancements to the creatives form.

Fixes issues introduced in #1227

app/controllers/creatives_controller.rb
@@ -29,13 +29,15 @@ class CreativesController < ApplicationController
 
   def create
     @creative = current_user.creatives.build(creative_params)
+    images_valid = creative_image_params_validation.valid?
 
     respond_to do |format|
-      if @creative.save
+      if @creative.save && images_valid
         @creative.assign_images(creative_image_params)
         format.html { redirect_to @creative, notice: "Creative was successfully created." }
         format.json { render :show, status: :created, location: @creative }
       else
+        @creative.errors.merge! creative_image_params_validation.errors
         format.html { render :new }
         format.json { render json: @creative.errors, status: :unprocessable_entity }
       end
@@ -43,14 +45,17 @@ class CreativesController < ApplicationController
   end
 
   def update
+    images_valid = creative_image_params_validation.valid?
+
     respond_to do |format|
-      if @creative.update(creative_params)
+      if @creative.update(creative_params) && images_valid
         @creative.assign_images creative_image_params
         @creative.update_columns status: ENUMS::CREATIVE_STATUSES::PENDING unless authorized_user(true).can_edit_creative?(@creative)
 
         format.html { redirect_to @creative, notice: "Creative was successfully updated." }
         format.json { render :show, status: :ok, location: @creative }
       else
+        @creative.errors.merge! creative_image_params_validation.errors
         format.html { render :edit }
         format.json { render json: @creative.errors, status: :unprocessable_entity }
       end
@@ -106,6 +111,10 @@ class CreativesController < ApplicationController
 
   def creative_image_params
     params.require(:creative).permit(:icon_blob_id, :small_blob_id, :large_blob_id, :wide_blob_id, :sponsor_blob_id)
+  end
+
+  def creative_image_params_validation
+    @creative_image_params_validation ||= CreativeImageParamsValidator.new(creative_image_params)
   end
 
   def authorize_new!

app/javascript/controllers/creative_form_controller.js
@@ -7,9 +7,11 @@ import { isEmpty } from 'lodash'
 export default class extends Controller {
   static targets = [
     'adLength',
+    'ctaLength',
     'copyWarningWrapper',
     'copyWrapper',
     'creativeType',
+    'inputCta',
     'inputBody',
     'inputHeadline',
     'inputLargeBlobId',
@@ -21,7 +23,6 @@ export default class extends Controller {
     'previewHeadline',
     'previewImageUrl',
     'previewSponsorImage',
-    'sponsorForm',
     'standardForm'
   ]
 
@@ -32,19 +33,12 @@ export default class extends Controller {
   initialize () {
     this.setHeadline()
     this.setBody()
-    this.setSmallImage()
+    // this.setIconImage()
+    // this.setSmallImage()
     this.setLargeImage()
-    this.setWideImage()
-    this.setSponsorImage()
+    // this.setWideImage()
     this.setTemplateTheme()
     this.checkLimit()
-    this.standardFormFields = this.standardFormTarget.querySelector(
-      '#standard-form-fields'
-    )
-    this.sponsorFormFields = this.sponsorFormTarget.querySelector(
-      '#sponsor-form-fields'
-    )
-    this.initCreativeType()
   }
 
   checkLimit () {
@@ -53,8 +47,10 @@ export default class extends Controller {
       this.previewBodyTarget.textContent.length +
       1
     this.setAdLengthCount(adLength)
+    const ctaLength = this.inputCtaTarget.value.length
+    this.setCtaLengthCount(ctaLength)
 
-    if (adLength > 85) {
+    if (adLength > 85 || ctaLength > 20) {
       this.copyWrapperTarget.classList.add('over-limit')
       this.copyWarningWrapperTarget.classList.remove('text-secondary')
       this.copyWarningWrapperTarget.classList.add('text-danger')
@@ -67,6 +63,10 @@ export default class extends Controller {
 
   setAdLengthCount (adLength) {
     this.adLengthTarget.textContent = adLength
+  }
+
+  setCtaLengthCount (ctaLength) {
+    this.ctaLengthTarget.textContent = ctaLength
   }
 
   setHeadline () {
@@ -90,6 +90,10 @@ export default class extends Controller {
     this.checkLimit()
   }
 
+  setIconImage () {
+    // Implement when we are able to display compatible templates
+  }
+
   setSmallImage () {
     // Implement when we are able to display compatible templates
   }
@@ -109,35 +113,7 @@ export default class extends Controller {
     // Implement when we are able to display compatible templates
   }
 
-  setSponsorImage () {
-    const target = this.inputSponsorBlobIdTarget
-    const blobId = parseInt(target.value, 10)
-    if (!isNaN(blobId)) {
-      const selectedOption = target.options[target.selectedIndex]
-      this.previewSponsorImageTarget.src = selectedOption.dataset.imageUrl
-    } else {
-      this.previewImageUrlTarget.src = 'about:blank'
-    }
-  }
-
   setTemplateTheme () {
     console.log('setTemplateTheme')
-  }
-
-  setCreativeType (event) {
-    const { creativeType } = event.target.dataset
-    this.creativeTypeTarget.value = creativeType
-    this.initCreativeType()
-  }
-
-  initCreativeType () {
-    const creativeType = this.creativeTypeTarget.value
-    if (creativeType === 'standard') {
-      this.sponsorFormFields.remove()
-      this.standardFormTarget.appendChild(this.standardFormFields)
-    } else if (creativeType === 'sponsor') {
-      this.standardFormFields.remove()
-      this.sponsorFormTarget.appendChild(this.sponsorFormFields)
-    }
   }
 }

app/models/creative.rb
@@ -23,7 +23,7 @@
 #
 
 class Creative < ApplicationRecord
-  MAX_COPY_LENGTH = 85
+  MAX_AD_COPY_LENGTH = 85
 
   # extends ...................................................................
   # includes ..................................................................
@@ -48,12 +48,8 @@ class Creative < ApplicationRecord
   validates :name, length: {maximum: 255, allow_blank: false}
   validates :status, inclusion: {in: ENUMS::CREATIVE_STATUSES.values}
   validates :creative_type, inclusion: {in: ENUMS::CREATIVE_TYPES.values}
-  validate :validate_icon_image
-  validate :validate_small_image
-  validate :validate_wide_image
-  validate :validate_large_image
   validate :validate_status_change
-  validate :validate_copy_length
+  validate :validate_ad_copy_length
 
   # callbacks .................................................................
   after_commit :touch_campaigns, on: [:update]
@@ -197,29 +193,9 @@ class Creative < ApplicationRecord
     end
   end
 
-  def validate_icon_image
-    return if icon_image
-    errors.add :base, "please select an icon image"
-  end
-
-  def validate_small_image
-    return if small_image
-    errors.add :base, "please select a small image"
-  end
-
-  def validate_wide_image
-    return if wide_image
-    errors.add :base, "please select a wide image"
-  end
-
-  def validate_large_image
-    return if large_image
-    errors.add :base, "please select a large image"
-  end
-
-  def validate_copy_length
-    return unless body.length + cta.length + headline.length > MAX_COPY_LENGTH
-    errors.add :base, "headline, body, and call to action cannot exceed a combined #{MAX_COPY_LENGTH} characters."
+  def validate_ad_copy_length
+    return unless body.length + headline.length + 1 > MAX_AD_COPY_LENGTH
+    errors.add :base, "ad headline, and body cannot exceed a combined #{MAX_AD_COPY_LENGTH} characters."
   end
 
   def validate_destroyable

app/validators/creative_image_params_validator.rb
@@ -1,0 +1,20 @@
+#
+# Used in the CreativesController to correctly verify that all image params
+# are present since images are not assigned until after the creative is saved
+#
+class CreativeImageParamsValidator
+  include ActiveModel::Validations
+
+  attr_accessor :icon_image, :small_image, :large_image, :wide_image
+  validates :icon_image, presence: {message: "must be selected"}
+  validates :small_image, presence: {message: "must be selected"}
+  validates :large_image, presence: {message: "must be selected"}
+  validates :wide_image, presence: {message: "must be selected"}
+
+  def initialize(params)
+    @icon_image = params[:icon_blob_id]
+    @small_image = params[:small_blob_id]
+    @large_image = params[:large_blob_id]
+    @wide_image = params[:wide_blob_id]
+  end
+end

app/views/creatives/_form.html.erb
@@ -33,11 +33,12 @@
           <div class="row">
             <div class="copy-wrapper col-12 p-0 py-2 mb-2" data-target="creative-form.copyWrapper">
               <div class="text-center pb-2 text-dark" data-target="creative-form.copyWarningWrapper">
-                Your ad copy is <span class="actual-length text-dark" data-target="creative-form.adLength">0</span> of <strong class="text-dark">85</strong> characters
+                <span class="d-block">Your ad copy is <span class="actual-length text-dark" data-target="creative-form.adLength">0</span> of <strong class="text-dark">85</strong> characters</span>
+                <span class="d-block">Your Call to Action copy is <span class="actual-length text-dark" data-target="creative-form.ctaLength">0</span> of <strong class="text-dark">20</strong> characters</span>
               </div>
-              <%= tag.div f.input(:headline, required: true, input_html: {data: {target: "creative-form.inputHeadline", action: "change->creative-form#setHeadline keyup->creative-form#setHeadline"}}), class: "col-12 mb-3" %>
-              <%= tag.div f.input(:body, required: true, input_html: {rows: 2, data: {target: "creative-form.inputBody", action: "change->creative-form#setBody keyup->creative-form#setBody"}}), class: "col-12 mb-3" %>
-              <%= tag.div f.input(:cta, label: "Call to Action", required: true, placeholder: "e.g. Learn more", hint: "Only used with the `sponsored-text` template"), class: "col-12 mb-3" %>
+              <%= tag.div f.input(:headline, required: true, placeholder: "Headline 🚀", input_html: {data: {target: "creative-form.inputHeadline", action: "change->creative-form#setHeadline keyup->creative-form#setHeadline"}}), class: "col-12 mb-3" %>
+              <%= tag.div f.input(:body, required: true, placeholder: "Your body will go here. You can use an emoji to help catch their eye 🤪", input_html: {rows: 2, data: {target: "creative-form.inputBody", action: "change->creative-form#setBody keyup->creative-form#setBody"}}), class: "col-12 mb-3" %>
+              <%= tag.div f.input(:cta, label: "Call to Action", required: true, placeholder: "e.g. Learn more", hint: "Only used with the `sponsored-text` template", input_html: {data: {target: "creative-form.inputCta", action: "change->creative-form#checkLimit keyup->creative-form#checkLimit"}}), class: "col-12 mb-3" %>
             </div>
             <% if missing_image_formats? %>
               <h4><%= missing_image_formats_alert[:heading] %></h4>

test/validators/creative_image_params_validator_test.rb
@@ -1,0 +1,23 @@
+require "test_helper"
+
+class CreativeImageParamsValidatorTest < ActiveSupport::TestCase
+  test "should return ActiveModel::Errors object" do
+    validation = CreativeImageParamsValidator.new(ActionController::Parameters.new({icon_blob_id: 1}))
+    assert validation.errors.is_a? ActiveModel::Errors
+  end
+
+  test "should be invalid with missing image params" do
+    validation = CreativeImageParamsValidator.new(ActionController::Parameters.new({icon_blob_id: 1, small_blob_id: 2}))
+    assert_not validation.valid?
+    assert_equal [:large_image, :wide_image], validation.errors.keys
+    assert_equal ["Large image must be selected", "Wide image must be selected"], validation.errors.full_messages
+  end
+
+  test "should be valid with correct image params" do
+    validation = CreativeImageParamsValidator.new(
+      ActionController::Parameters.new({icon_blob_id: 1, small_blob_id: 2, wide_blob_id: 3, large_blob_id: 4})
+    )
+    assert validation.valid?
+    assert validation.errors.empty?
+  end
+end