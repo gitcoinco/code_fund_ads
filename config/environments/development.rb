5083430de4af788b4e88f072b1d2ad100ef60e5f
Eric Berry <coderberry@gmail.com>
2020-05-05 17:32:41 -0600

    Rollback prior commit (#1257)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,9 +45,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.delivery_method = :letter_opener
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
