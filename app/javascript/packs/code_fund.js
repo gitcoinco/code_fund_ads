15336d1b6f8ee1c0338dd7c5e4c464dee1022931
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-07 00:39:49 -0400

    correctly polyfill features in the older browsers (#1259)

--- app/javascript/packs/code_fund.js
+++ app/javascript/packs/code_fund.js
@@ -1,4 +1,12 @@
-require.context('../images', true, /\.(?:png|jpg|gif|ico|svg)$/)
+// Needed for Babel 7
+// https://github.com/rails/webpacker/blob/master/docs/es6.md#babel
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import '../controllers'
 import '../src'
 import '../themes/current'
+
+// Require all images in the /app/javascript/images directory
+// https://github.com/rails/webpacker/blob/master/docs/assets.md#link-in-your-rails-views
+require.context('../images', true, /\.(?:png|jpg|gif|ico|svg)$/)
--- app/javascript/packs/code_fund_ad.js
+++ app/javascript/packs/code_fund_ad.js
@@ -1,2 +1,6 @@
-require.context('../images', true, /\.(?:png|jpg|gif|ico|svg)$/)
+// Needed for Babel 7
+// https://github.com/rails/webpacker/blob/master/docs/es6.md#babel
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import '../advertisements'
