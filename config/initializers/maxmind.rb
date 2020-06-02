6ee6e62bb29a4f4d5d1b50ac29336614f65a0713
Andrew Mason <andrewmcodes@protonmail.com>
2020-06-02 12:40:02 -0400

    fix: :bug: Stop maxmind from running in development (#1315)

--- config/initializers/maxmind.rb
+++ config/initializers/maxmind.rb
@@ -1,5 +1,1 @@
-if Rails.env.production? || ENV["CI"] == "true"
-  DownloadAndExtractMaxmindFileJob.perform_now
-else
-  DownloadAndExtractMaxmindFileJob.perform_later
-end
+DownloadAndExtractMaxmindFileJob.perform_now if Rails.env.production? || ENV["CI"] == "true"
