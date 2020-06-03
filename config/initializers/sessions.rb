96723311010a9f0e29d425890e405428c531ccaa
Руслан Корнев <oganer@gmail.com>
2020-06-03 17:23:27 +0300

    chore: :wrench: Makes session works without caching-dev.txt (#1313)

--- config/initializers/sessions.rb
+++ config/initializers/sessions.rb
@@ -1,5 +1,7 @@
 # Using redis to manage sessions (same cache appliance as standard rails caching)
 # IMPORTANT: "expire_after" should match "timeout_in" config in: config/initializers/devise.rb
-Rails.application.config.session_store ActionDispatch::Session::CacheStore,
-  key: "_code_fund_ads_session",
-  expire_after: 2.weeks
+unless Rails.application.config.cache_store == :null_store
+  Rails.application.config.session_store ActionDispatch::Session::CacheStore,
+    key: "_code_fund_ads_session",
+    expire_after: 2.weeks
+end
